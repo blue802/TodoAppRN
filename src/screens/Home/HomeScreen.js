2dabee2ad775a6199f5ce4b22e9472f92f649061
blue0802 <quocbaoit0802@gmail.com>
2020-11-26 20:33:20 +0700

    fixed warning cannot connect to the metro server

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {View, Text,TouchableOpacity,Image} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 
 import {useUser} from '../../UserProvider';
 import styles from './styles';
-import Add from "./images/plus.png";
+import Add from './images/plus.png';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
   const [{user}, dispath] = useUser();
-  const {navigation} = props;
-  return ( 
+  return (
     <View style={styles.container}>
-            <Text>Home</Text>
-            <Text>Hello {user.displayName}</Text>
-            <TouchableOpacity style={styles.addButton} onPress={()=>{navigation.navigate("CreateTask",
-                                                                              {navigation:props.navigation})}}>
-                <Image source={Add} style={{width:56,height:56}}/>
-            </TouchableOpacity>
+      <Text>Home</Text>
+      <Text>Hello {user.displayName}</Text>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+          navigation.navigate('CreateTask');
+        }}>
+        <Image source={Add} style={{width: 56, height: 56}} />
+      </TouchableOpacity>
     </View>
-    );
+  );
 };
 
 export default HomeScreen;
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -2,17 +2,17 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-      flex:1
+    flex: 1,
+    backgroundColor: '#FAF9FF',
   },
-  addButton:
-  {
-      elevation:40,
-      width: 64,
-      height: 64,
-      position: "absolute",
-      bottom: 6,
-      right: 6,
-  }
-})
+  addButton: {
+    elevation: 40,
+    width: 64,
+    height: 64,
+    position: 'absolute',
+    bottom: 6,
+    right: 6,
+  },
+});
 
 export default styles;
